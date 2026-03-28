8f40051977fd0b54111171c4c4b83f6bd8ea6391
[dyad] <git@dyad.sh>
2026-03-28 19:37:53 -0300

    [dyad] Consolidando a estrutura do projeto em /src para ativar a Sidebar em todas as telas. - wrote 2 file(s), deleted 2 file(s)

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -11,6 +10,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
